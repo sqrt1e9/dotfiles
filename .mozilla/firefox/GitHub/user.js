e3904ad85265c445689e2ed21b6e9bf912eaed5e
Arvind Badri <barvind.dev@outlook.com>
2025-12-06 00:45:37 -0600

    dotfiles: 2025-12-06 00:45

--- .mozilla/firefox/GitHub/user.js
+++ .mozilla/firefox/GitHub/user.js
@@ -1,2 +1,3 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.tabs.allow_transparent_browser", true);
+user_pref("browser.display.use_document_fonts", 0);
