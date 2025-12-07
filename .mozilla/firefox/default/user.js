49b6099c288080d030090d6760456349a453b234
Arvind Badri <barvind.dev@outlook.com>
2025-12-07 01:20:47 -0600

    dotfiles: 2025-12-07 01:20

--- .mozilla/firefox/default/user.js
+++ .mozilla/firefox/default/user.js
@@ -1,3 +1,8 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.tabs.allow_transparent_browser", true);
 user_pref("browser.display.use_document_fonts", 0);
+user_pref("svg.content-properties.content.enabled", true);
+user_pref("layers.acceleration.force-enabled", true);
+user_pref("gfx.webrender.all", true);
+user_pref("gfx.webrender.enabled", true);
+user_pref("layout.css.backdrop-filter.enabled", true);
