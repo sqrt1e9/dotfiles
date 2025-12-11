a374e4927d087ee412ee338f7390dd0af2225987
Arvind Badri <barvind.dev@outlook.com>
2025-12-10 21:07:52 -0600

    Exclucded all fireefox

--- .mozilla/firefox/ChatGPT/user.js
+++
@@ -1,3 +0,0 @@
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("browser.tabs.allow_transparent_browser", true);
-user_pref("browser.display.use_document_fonts", 0);
