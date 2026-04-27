7725fd5785e2241ca03363fda5325379c095721e
Arvind Badri <barvind.dev@outlook.com>
2026-04-26 23:15:46 -0500

    masOS Catppuccin hybrid

--- .local/bin/wallpaper-select.sh
+++ .local/bin/wallpaper-select.sh
@@ -1,90 +1,81 @@
 #!/usr/bin/env bash
-## /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
-# This script for selecting wallpapers (SUPER W)
 
-# Wallpapers Path
 wallpaperDir="$HOME/.local/share/backgrounds"
 themesDir="$HOME/.config/rofi"
 
-# Transition config
-FPS=60
-TYPE="any"
-DURATION=3
-BEZIER="0.4,0.2,0.4,1.0"
-
-# Check if swaybg is running
 if pidof swaybg > /dev/null; then
-	pkill swaybg
+    pkill swaybg
 fi
 
-# Retrieve image files as a list
-PICS=($(find -L "${wallpaperDir}" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) | sort))
+mapfile -t PICS < <(
+    find -L "$wallpaperDir" -type f \
+        \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) \
+        | sort
+)
 
-# Use date variable to increase randomness
+if [[ ${#PICS[@]} -eq 0 ]]; then
+    notify-send "Wallpaper error" "No wallpapers found in $wallpaperDir"
+    exit 1
+fi
+
 randomNumber=$(( ($(date +%s) + RANDOM) + $$ ))
 randomPicture="${PICS[$(( randomNumber % ${#PICS[@]} ))]}"
 randomChoice="[${#PICS[@]}] Random"
 
-# Rofi command
 rofiCommand="rofi -show -dmenu -theme ${themesDir}/wallpaper-select.rasi"
 
-# Execute command according to the wallpaper manager
 executeCommand() {
-	file="$1"
-	if command -v hyprctl >/dev/null 2>&1; then
-	    ~/.local/bin/hyprwall-init.sh "$file"
-	else
-		echo "No supported wallpaper setter found (swaybg or Hyprpaper)."
-		exit 1
-	fi
+    file="$1"
+
+    if command -v hyprctl >/dev/null 2>&1; then
+        "$HOME/.local/bin/hyprwall-init.sh" "$file"
+    else
+        echo "No supported wallpaper setter found."
+        exit 1
+    fi
 }
 
-# Show the images
 menu() {
-	printf "%s\n" "$randomChoice"
+    printf "%s\n" "$randomChoice"
 
-	for i in "${!PICS[@]}"; do
-		if [[ "${PICS[$i]}" != *.gif ]]; then
-			printf "%s\x00icon\x1f%s\n" \
-				"$(basename "${PICS[$i]}" | cut -d. -f1)" \
-				"${PICS[$i]}"
-		else
-			printf "%s\n" "$(basename "${PICS[$i]}")"
-		fi
-	done
+    for i in "${!PICS[@]}"; do
+        if [[ "${PICS[$i]}" != *.gif ]]; then
+            printf "%s\x00icon\x1f%s\n" \
+                "$(basename "${PICS[$i]}" | cut -d. -f1)" \
+                "${PICS[$i]}"
+        else
+            printf "%s\n" "$(basename "${PICS[$i]}")"
+        fi
+    done
 }
 
-# Execution
 main() {
-	choice=$(menu | ${rofiCommand})
+    choice=$(menu | ${rofiCommand})
 
-	# No choice case
-	if [[ -z "$choice" ]]; then
-		exit 0
-	fi
+    if [[ -z "$choice" ]]; then
+        exit 0
+    fi
 
-	# Random choice case
-	if [[ "$choice" == "$randomChoice" ]]; then
-		executeCommand "${randomPicture}"
-		return 0
-	fi
-	# Find selected file
-	for file in "${PICS[@]}"; do
-		if [[ "$(basename "$file" | cut -d. -f1)" == "$choice" ]]; then
-			selectedFile="$file"
-			break
-		fi
-	done
+    if [[ "$choice" == "$randomChoice" ]]; then
+        executeCommand "$randomPicture"
+        return 0
+    fi
 
-	# Check the file and execute
-	if [[ -n "$selectedFile" ]]; then
-		executeCommand "${selectedFile}"
-	else
-		echo "Image not found."
-		exit 1
-	fi
+    selectedFile=""
+
+    for file in "${PICS[@]}"; do
+        if [[ "$(basename "$file" | cut -d. -f1)" == "$choice" || "$(basename "$file")" == "$choice" ]]; then
+            selectedFile="$file"
+            break
+        fi
+    done
+
+    if [[ -n "$selectedFile" ]]; then
+        executeCommand "$selectedFile"
+    else
+        notify-send "Wallpaper error" "Image not found."
+        exit 1
+    fi
 }
 
-# Check if rofi is already running
 main
-
